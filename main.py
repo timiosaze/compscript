697d70c75996a88ed228d538060d80fc1072333b
timiosaze <timiade1993@gmail.com>
2022-09-23 15:16:51 +0100

    lop

--- main.py
+++ main.py
@@ -197,6 +197,8 @@ def saveData(file, proxy):
                 break
             except requests.exceptions.ProxyError:
                 print("Proxy Error Encountered: Reloading")
+        ip = response.json()['ip']
+        print('Your public IP is:', ip)     
         soup = BeautifulSoup(response.text, "lxml")
         div = soup.find('script',attrs = {'id':'__NEXT_DATA__'})
         print(response.text)
