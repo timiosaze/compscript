e3b9df687ceddca7a1177ff17f628419e257d194
timiosaze <timiade1993@gmail.com>
2022-09-23 14:45:02 +0100

    lop

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def clear_states():
     f.truncate(0) # need '0' when using r+
 
 # def proxies_list():
-#     headers={'User-Agent': chrome_ua}
+#     headers={'User-Agent': ua.chrome}
 #     response = requests.get('https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt', headers=headers)
 #     with open("response.txt", "w") as f:
 #         f.write(response.text)
@@ -106,7 +106,7 @@ def extract(proxy):
     # auth = HTTPProxyAuth("ahmdevnb", "d6n2kw7b9l03")
     while True:
         try:
-            r = requests.get('https://www.comparis.ch/immobilien/result/list?requestobject=%7B%22DealType%22%3A20%2C%22SiteId%22%3A0%2C%22RootPropertyTypes%22%3A%5B%5D%2C%22PropertyTypes%22%3A%5B%5D%2C%22RoomsFrom%22%3Anull%2C%22RoomsTo%22%3Anull%2C%22FloorSearchType%22%3A0%2C%22LivingSpaceFrom%22%3Anull%2C%22LivingSpaceTo%22%3Anull%2C%22PriceFrom%22%3Anull%2C%22PriceTo%22%3Anull%2C%22ComparisPointsMin%22%3A0%2C%22AdAgeMax%22%3A0%2C%22AdAgeInHoursMax%22%3Anull%2C%22Keyword%22%3A%22%22%2C%22WithImagesOnly%22%3Anull%2C%22WithPointsOnly%22%3Anull%2C%22Radius%22%3A%2220%22%2C%22MinAvailableDate%22%3A%221753-01-01T00%3A00%3A00%22%2C%22MinChangeDate%22%3A%221753-01-01T00%3A00%3A00%22%2C%22LocationSearchString%22%3A%22Z%C3%BCrich%22%2C%22Sort%22%3A11%2C%22HasBalcony%22%3Afalse%2C%22HasTerrace%22%3Afalse%2C%22HasFireplace%22%3Afalse%2C%22HasDishwasher%22%3Afalse%2C%22HasWashingMachine%22%3Afalse%2C%22HasLift%22%3Afalse%2C%22HasParking%22%3Afalse%2C%22PetsAllowed%22%3Afalse%2C%22MinergieCertified%22%3Afalse%2C%22WheelchairAccessible%22%3Afalse%2C%22LowerLeftLatitude%22%3Anull%2C%22LowerLeftLongitude%22%3Anull%2C%22UpperRightLatitude%22%3Anull%2C%22UpperRightLongitude%22%3Anull%7D', proxies={'http':proxy, 'https':proxy},headers={"Authorization": "Token p55g59nll97lhy5rt9hmshlpqq7v7qnf43fwmrzq"})
+            r = requests.get('https://www.comparis.ch/immobilien/result/list?requestobject=%7B%22DealType%22%3A20%2C%22SiteId%22%3A0%2C%22RootPropertyTypes%22%3A%5B%5D%2C%22PropertyTypes%22%3A%5B%5D%2C%22RoomsFrom%22%3Anull%2C%22RoomsTo%22%3Anull%2C%22FloorSearchType%22%3A0%2C%22LivingSpaceFrom%22%3Anull%2C%22LivingSpaceTo%22%3Anull%2C%22PriceFrom%22%3Anull%2C%22PriceTo%22%3Anull%2C%22ComparisPointsMin%22%3A0%2C%22AdAgeMax%22%3A0%2C%22AdAgeInHoursMax%22%3Anull%2C%22Keyword%22%3A%22%22%2C%22WithImagesOnly%22%3Anull%2C%22WithPointsOnly%22%3Anull%2C%22Radius%22%3A%2220%22%2C%22MinAvailableDate%22%3A%221753-01-01T00%3A00%3A00%22%2C%22MinChangeDate%22%3A%221753-01-01T00%3A00%3A00%22%2C%22LocationSearchString%22%3A%22Z%C3%BCrich%22%2C%22Sort%22%3A11%2C%22HasBalcony%22%3Afalse%2C%22HasTerrace%22%3Afalse%2C%22HasFireplace%22%3Afalse%2C%22HasDishwasher%22%3Afalse%2C%22HasWashingMachine%22%3Afalse%2C%22HasLift%22%3Afalse%2C%22HasParking%22%3Afalse%2C%22PetsAllowed%22%3Afalse%2C%22MinergieCertified%22%3Afalse%2C%22WheelchairAccessible%22%3Afalse%2C%22LowerLeftLatitude%22%3Anull%2C%22LowerLeftLongitude%22%3Anull%2C%22UpperRightLatitude%22%3Anull%2C%22UpperRightLongitude%22%3Anull%7D', proxies={'http':proxy, 'https':proxy}, headers={'User-Agent': ua.chrome})
             # print(str(r.status_code) + ":  -> " + proxy)
             if(r.status_code == 200):
                 pcount = pcount + 1
@@ -135,7 +135,7 @@ def getAllBuyProperties(proxy):
                 'http' :proxy,
                 'https':proxy,
                 }
-    headers={"Authorization": "Token p55g59nll97lhy5rt9hmshlpqq7v7qnf43fwmrzq"}
+    headers={'User-Agent': ua.chrome}
    
     with open('/home/compscript/urls.txt', 'r') as reader:
         for line in reader.readlines():
@@ -198,7 +198,7 @@ def saveData(file, proxy):
        
         while True:
             try:
-                response = requests.get('https://www.comparis.ch/immobilien/marktplatz/details/show/' + new_id + '',proxies={'http':proxy},headers={"Authorization": "Token p55g59nll97lhy5rt9hmshlpqq7v7qnf43fwmrzq"})
+                response = requests.get('https://www.comparis.ch/immobilien/marktplatz/details/show/' + new_id + '',proxies={'http':proxy}, headers={'User-Agent': ua.chrome})
                 break
             except requests.exceptions.ProxyError:
                 print("Proxy Error Encountered: Reloading")
